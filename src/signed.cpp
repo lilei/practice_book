7074f5448cbf6d73d112f66685e0d1856038587e
lilei <lilei5678@163.com>
2017-08-17 20:53:55 +0800

    两个数值运算的例子

---
+++ src/signed.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <stdlib.h>
+
+TEST(signed, unsighed_minus)
+{
+    unsigned int a = 1;
+    unsigned int b = 2;
+    std::cout << b - a << std::endl;
+
+    //无符号数的减法是会溢出的,但是取绝对值却可以得到正确的结果
+    std::cout << a - b << std::endl;
+
+
+    std::cout << (double)a - (double)b << std::endl;
+
+    std::cout << abs((double)a - (double)b) << std::endl;
+    std::cout << abs(a - b) << std::endl;
+}
+
+TEST(signed, mod)
+{
+    //负数是可以做模运算并得到正确结果的
+    size_t a = 0;
+    std::cout << (a - 1) % 256 << std::endl;
+}
--- src/test.cpp
+++ src/test.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    testing::GTEST_FLAG(filter) = "time.clock";
+    testing::GTEST_FLAG(filter) = "signed.mod";
     //testing::GTEST_FLAG(repeat) = 1000;
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
