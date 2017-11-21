c2124cf2c0d19edce748a7d1dc2672b5439243fc
lilei <lilei5678@163.com>
2017-11-21 10:27:22 +0800

    重载new delete，定义placement new placement delete

--- src/test_new.cpp
+++ src/test_new.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <new>
 
 
 TEST(new,override)
@@ -8,18 +9,66 @@ TEST(new,override)
     public:
         A()
         {
-            b_ = false;
         }
         ~A()
         {
         }
-        void* operator new(size_t size)
+        static void* operator new(size_t size)
         {
+            printf("operator new in class A\n");
             return ::operator new (size);
         }
 
-        bool b_;
+        static void operator delete(void* ptr)
+        {
+            printf("operator delete in class A\n");
+            ::operator delete (ptr);
+        }
     };
 
     auto a = new A;
+    delete a;
+}
+
+TEST(new,placement)
+{
+    class A 
+    {
+    public:
+        A()
+        {
+        }
+        ~A()
+        {
+        }
+        static void* operator new(size_t size)
+        {
+            printf("operator new in class A\n");
+            return ::operator new (size);
+        }
+
+        static void operator delete(void* ptr)
+        {
+            printf("operator delete in class A\n");
+        }
+
+        static void* operator new(size_t size,void* ptr)
+        {
+            printf("placement new in class A\n");
+            return ptr;
+        }
+
+        static void operator delete(void* ptr,void*)
+        {
+            printf("placement delete in class A\n");
+        }
+
+    };
+
+    char buf[4] = { 0 };
+    auto a = new (&buf[0])A;
+    delete a;
+
+    auto b = new A;
+    delete b;
 }
