3e7ffa8bfe0bd6860f9df36aa53a5d5195083dba
lilei <lilei5678@163.com>
2017-11-24 14:35:39 +0800

    定义几个常见的HTML标签

--- src/html_element.h
+++ src/html_element.h
@@ -5,50 +5,17 @@
 namespace html
 { 
 
-class document :public Node
-{
-public:
-    document() :Node("html")
-    {
-        append_child("head").append_child("title");
-        append_child("body");
-    }
-};
-
-class div :public Node
-{
-public:
-    div() :Node("div") { }
-};
-
-class img :public Node
-{
-public:
-    img(const char* src) 
-        :Node("img") 
-    { 
-        attri_[src] = src;
-    }
-};
-
-class Td :public Node
-{
-public:
-    Td(const char* text) 
-        :Node("td",text) {}
-};
-
-class Tr :public Node
-{
-public:
-    Tr() :Node("tr") {}
-};
-
-class Table :public Node
-{
-public:
-    Table() :Node("table") {}
-};
+class Html  :public Node { public: Html()  :Node("html")  {} };
+class Head  :public Node { public: Head()  :Node("head")  {} };
+class Body  :public Node { public: Body()  :Node("body")  {} };
+class Title :public Node { public: Title() :Node("title") {} };
+class Div   :public Node { public: Div()   :Node("div")   {} };
+class Img   :public Node { public: Img()   :Node("img")   {} };
+class Table :public Node { public: Table() :Node("table") {} };
+class Tr    :public Node { public: Tr()    :Node("tr")    {} };
+class Td    :public Node { public: Td()    :Node("td")    {} };
+class Span  :public Node { public: Span()  :Node("span")  {} };
+class Hr    :public Node { public: Hr()    :Node("hr")    {} };
 
 } // end namespace
 
