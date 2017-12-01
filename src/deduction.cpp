41708d43c148ba15344c4248b2d83c516bcc39cd
lilei <lilei5678@163.com>
2017-12-01 18:43:58 +0800

    类型推导的例子

--- src/deduction.cpp
+++ src/deduction.cpp
@@ -1,8 +1,15 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-//typeid 并不能返回 const refrence等信息，只能返回类型和指针类型
-//只是在vs2017下测试，gcc尚未测试 fuck windows10 LSB 
+TEST(deduction,IDE_cursor_over)
+{
+    const int a = 42;
+    auto x = a;
+    auto y = &a;
+}
+
+//typeid用于在运行期获取类型信息
+//但typeid 并不能返回 const refrence等信息，只能返回类型和指针类型
 class DA {};
 TEST(deduction,typeid)
 {
@@ -21,58 +28,70 @@ TEST(deduction,typeid)
         << typeid(da1).name() << std::endl;
 }
 
+//所以我们用下面这种方式来在编译器
+//声明一个模板类TD，但不定义
+//编译器会在编译器错误里给出x,y的准确类型
+template<typename T>
+class TD;
+TEST(deduction,complier_diag)
+{
+    const int a = 42;
+    auto x = a;
+    auto y = &a;
+
+    /*
+    TD<decltype(x)> xType;
+    TD<decltype(y)> xType;
+    */
+}
+
+
 /*
 template <typename T>
 void f(paramType param);
 f(expr)
 T的类型推导不仅仅取决于expr的类型，还取决于param的类型(paramType)
+类型推导有如下三种情况:
+case 1.paramType是引用或指针,但不是Universal引用
+case 2.paramType是Universal 引用
+case 3.ParamType不是指针也不是引用
 */
 
 template <typename T>
-auto func1(T& n)
+void func1(T& n)
 {
-   std::cout << "type T: " << typeid(T).name() << " type param: " << typeid(n).name()<<std::endl;
-   return std::make_pair(typeid(T).name(),typeid(T&).name());
+   //TD<decltype(T)> xType;
+   TD<decltype(n)> yType;
 }
-
-TEST(deduction,param_type_ref_of_pointer1)
+TEST(deduction,paramtype_case1)
 {
     int x = 27;
     const int cx = x;
     const  int& rx = x;
-    func1(x);
-    func1(cx);
-    func1(rx);
-    //EXPECT_EQ(std::make_pair("int","int"),func1(x));
-    /*
-    EXPECT_EQ("int",func1(cx));
+    //func1(x);      //T: int parameType: int
+    //func1(cx);     //T: int parameType: const int
+    func1(rx);     //T: int parameType: const int
+
     //这种情况下根据expr来推导T
     //cx的类型是一个引用，类型推倒时省略引用的部分，所以T的类型还是int 而不是 int&
-    EXPECT_EQ("int",func1(rx));
-    */
 }
 
 
 template <typename T>
 auto func2(const T& n)
 {
-   std::cout << "type T: " << typeid(T).name() << " type param: " << typeid(n).name()<<std::endl;
-   return std::make_pair(typeid(T).name(),typeid(T&).name());
+   TD<decltype(T)> xType;
+   TD<decltype(n)> yType;
 }
 
-TEST(deduction,param_type_ref_of_pointer)
+TEST(deduction,paramtype_case2)
 {
     int x = 27;
     const int cx = x;
     const  int& rx = x;
     std::cout << "----type--- " << typeid(rx).name() << std::endl;
-    func2(x);
-    func2(cx);
-    func2(rx);
-    /*
-    EXPECT_EQ("int",func2(x));
-    EXPECT_EQ("int",func2(cx));
-    EXPECT_EQ("int",func2(rx));
-    */
+    //func2(x);      //T: int parameType: int
+    //func2(cx);     //T: int parameType: int
+    //func2(rx);     //T: int parameType: int
 }
 
