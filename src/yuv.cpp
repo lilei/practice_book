fc1d085dd0f124b0bd9a821c2e81624b0027065f
lilei <lilei5678@163.com>
2017-12-08 11:47:57 +0800

    封装一个yuv处理的class

--- src/yuv.cpp
+++ src/yuv.cpp
@@ -2,38 +2,99 @@
 #include <fstream>
 #include <stdint.h>
 
-int yuv420_separate(std::ifstream& fin, int width, int height, int num)
+class YUV 
 {
-    std::ofstream fy("y.yuv",std::fstream::binary);
-    std::ofstream fu("u.yuv",std::fstream::binary);
-    std::ofstream fv("v.yuv",std::fstream::binary);
-
-    size_t pic_size = width * height * 3 / 2;
-    size_t size_y = pic_size * 2 / 3;
-    size_t size_u = pic_size / 6;
-    size_t size_v = pic_size / 6;
-    char *buffer = new char[pic_size];
-    
-    for (int i = 0; i < num; i++) 
+public:
+    enum YUV_FORMAT
     {
-        fin.read(buffer, size_y);
-        fy.write(buffer, size_y);
-
-        fin.read(buffer, size_u);
-        fu.write(buffer, size_u);
-
-        fin.read(buffer, size_v);
-        fv.write(buffer, size_v);
+        YUV_420,
+        YUV_444
+    };
+    YUV(std::ifstream& fin,int width,int height,YUV_FORMAT format)
+        :fin_(fin),width_(width),height_(height),format_(format)
+    {
     }
-    delete[] buffer;
-    return 0;
-}
 
-TEST(yuv,split)
-{
-    std::ifstream fin ("lena_256x256_yuv420p.yuv",std::fstream::binary);
-    if (fin)
+    void separate(std::ofstream &fy, std::ofstream &fu, std::ofstream& fv)
     {
-        yuv420_separate(fin,256,256,1);
+        if (!fy || !fu || !fv)
+        {
+            return;
+        }
+
+        size_t pic_size = 0;
+        size_t size_y   = 0;
+        size_t size_u   = 0;
+        size_t size_v   = 0;
+        char* buffer    = nullptr;
+
+        if (YUV_420 == format_)
+        {
+            pic_size = width_ * height_ * 3 / 2;
+            size_y   = pic_size * 2 / 3;
+            size_u   = pic_size / 6;
+            size_v   = pic_size / 6;
+        }
+        else if (YUV_444 == format_)
+        {
+            pic_size = width_ * height_;
+            size_y   = pic_size;
+            size_u   = pic_size;
+            size_v   = pic_size;
+        }
+        buffer = new char[pic_size];
+
+        for (;fin_ && !fin_.eof();)
+        {
+            fin_.read(buffer, size_y);
+            if (fin_.eof())
+            {
+                break;
+            }
+            fy.write(buffer, size_y);
+
+            fin_.read(buffer, size_u);
+            if (fin_.eof())
+            {
+                break;
+            }
+            fu.write(buffer, size_u);
+
+            fin_.read(buffer, size_v);
+            if (fin_.eof())
+            {
+                break;
+            }
+            fv.write(buffer, size_v);
+        }
+        delete[] buffer;
+    }
+private:
+    std::ifstream& fin_;
+    int width_;
+    int height_;
+    YUV_FORMAT  format_;
+};
+
+TEST(yuv,separate)
+{
+    std::ifstream fyuv420("lena_256x256_yuv420p.yuv", std::fstream::binary);
+    if (fyuv420)
+    {
+        YUV yuv(fyuv420,256,256,YUV::YUV_420);
+        std::ofstream fy("420_y.yuv",std::fstream::binary);
+        std::ofstream fu("420_u.yuv",std::fstream::binary);
+        std::ofstream fv("420_v.yuv",std::fstream::binary);
+        yuv.separate(fy,fu,fv);
+    }
+
+    std::ifstream fyuv444("lena_256x256_yuv444p.yuv", std::fstream::binary);
+    if (fyuv444)
+    {
+        YUV yuv(fyuv444,256,256,YUV::YUV_444);
+        std::ofstream fy("444_y.yuv",std::fstream::binary);
+        std::ofstream fu("444_u.yuv",std::fstream::binary);
+        std::ofstream fv("444_v.yuv",std::fstream::binary);
+        yuv.separate(fy,fu,fv);
     }
 }
