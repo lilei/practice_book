c7d6bf50459a588c708e8b55bed90660fc61f702
lilei <lilei5678@163.com>
2017-12-15 14:17:21 +0800

    解析字符串的链式写法

--- src/format.cpp
+++ src/format.cpp
@@ -1,50 +1,114 @@
 #include <gtest/gtest.h>
 #include <sstream>
 
-bool read_until(std::istringstream &ss, std::string &str,const char c)
+class stringparser
 {
-    char temp;
-    for (;!ss.eof();)
-    {
-        ss >> temp;
-        if (c == temp)
-        {
-            return true;
-        }
-        str += temp;
-    }
-    return false;
-}
+public:
+    stringparser(std::string str)
+        :s_(str) {}
 
-bool consume(std::istringstream &ss, const char c)
-{
-    char temp;
-    ss >> temp;
-    if (c == temp)
+    stringparser& read_until(std::string& got,const char until)
     {
-        return true;
+        char temp;
+        for (;!s_.eof();)
+        {
+            s_ >> temp;
+            if (until == temp)
+            {
+                return *this;
+            }
+            got += temp;
+        }
+        bad();
+        return *this;
     }
-    else
+
+    stringparser& read_int(int& got)
     {
-        return false;
+        s_ >> got;
+        if (s_.eof())
+        {
+            bad();
+        }
+        return *this;
     }
-}
+
+
+    stringparser& consume(char c)
+    {
+        char temp;
+        s_ >> temp;
+        if (c != temp)
+        {
+            bad();
+        }
+        return *this;
+    }
+
+    stringparser& consume(const std::string& str)
+    {
+        for (size_t i = 0;i < str.size();i++)
+        {
+            char temp = 0;
+            if (!s_ >> temp)
+            {
+                bad();
+            }
+            if (str.at(i) != temp)
+            {
+                bad();
+            }
+        }
+        return *this;
+    }
+
+    void end()
+    {
+        char temp;
+        s_ >> temp;
+        if (!s_.eof())
+        {
+            bad();
+        }
+    }
+
+    class exception
+    {
+    };
+private:
+    void bad()
+    {
+        throw exception();
+    }
+
+    std::istringstream s_;
+};
+
 
 TEST(format,urlpaser)
 {
     std::string url = "rtsp://127.0.0.1:8000/file/sessions/ad34wdwdaw12jbj232jnijiijwad2323gh";
-    std::istringstream ss (url);
+    stringparser parser(url);
 
     std::string proto;
     std::string ip;
     int port = 0;
     std::string rest;
 
-    read_until(ss, proto, ':');
-    consume(ss,'/');
-    consume(ss,'/');
-    read_until(ss, ip, ':');
-    ss >> port;
+    try
+    {
+        parser.read_until(proto, ':')
+            .consume('/')
+            .consume('/') 
+            .read_until(ip,':')
+            .read_int(port)
+            .consume("/file")
+            .consume("/sessions/");
+    }
+    catch (const stringparser::exception&)
+    {
+    }
+
 
     std::cout << "proto: " << proto << std::endl;
     std::cout << "ip: " << ip << std::endl;
