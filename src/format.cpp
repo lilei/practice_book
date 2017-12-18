14ca9900ed8669236c97e20ebd33b30bb2cf47c3
lilei <lilei5678@163.com>
2017-12-18 14:23:27 +0800

    stringbuffer增加向后看的能力； 新增一个urlparser专门用于解析带querystring的字符串。不过实现的时候各种try catch，感觉不够好

--- src/format.cpp
+++ src/format.cpp
@@ -19,21 +19,38 @@ public:
             }
             got += temp;
         }
+        got.clear();
         bad();
         return *this;
     }
 
     stringparser& read_int(int& got)
     {
-        s_ >> got;
-        if (s_.eof())
+        if (!(s_ >> got))
         {
             bad();
         }
         return *this;
     }
 
-    stringparser& consume(char c)
+    stringparser& read_len(std::string& got, const int len)
+    {
+        char *temp = new char[len];
+        if (!(s_.read(temp,len)))
+        {
+            bad();
+        }
+        got = temp;
+        return *this;
+    }
+
+    stringparser& read_to_pos(std::string& got, const int pos)
+    {
+        int len = pos - s_.tellg();
+        read_len(got,len);
+    }
+
+    stringparser& consume(const char c)
     {
         char temp;
         s_ >> temp;
@@ -61,14 +78,95 @@ public:
         return *this;
     }
 
-    void end()
+    bool test(const char c)
+    {
+        try
+        {
+            consume(c);
+        }
+        catch (const exception&)
+        {
+            s_.seekg(-1,std::ios_base::cur);
+            return false;
+        }
+        return true;
+    }
+
+    bool split(const char c,std::string& left,std::string& right)
+    {
+        try
+        {
+            read_until(left,c);
+        }
+        catch (const exception&)
+        {
+            return false;
+        }
+        s_ >> right;
+        return true;
+    }
+
+    bool split(const char c,std::vector<std::string>& strings)
+    {
+        std::string left;
+        std::string right;
+        if (!split(c, left, right))
+        {
+            return false;
+        }
+        strings.push_back(left);
+
+        while (split(c, left, right))
+        {
+            strings.push_back(left);
+        }
+
+        std::string rest;
+        s_ >> rest;
+
+        return true;
+    }
+
+    std::string rest()
+    {
+        std::string rest;
+        s_ >> rest;
+        return rest;
+    }
+
+    stringparser& begin()
+    {
+        s_.seekg(0);
+        return *this;
+    }
+
+    bool end()
     {
         char temp;
         s_ >> temp;
-        if (!s_.eof())
+        if (s_.eof())
         {
-            bad();
+            return true;
         }
+        else
+        {
+            return false;
+        }
+    }
+
+    size_t pos()
+    {
+        return (size_t)s_.tellg();
+    }
+
+    void goto_pos(size_t pos)
+    {
+        s_.seekg(pos);
+    }
+
+    void go_back(size_t n)
+    {
+        s_.seekg((int)(0-n),std::ios_base::cur);
     }
 
     class exception
@@ -83,8 +181,150 @@ private:
     std::istringstream s_;
 };
 
+class UrlParser  
+{
+public:
+    UrlParser(std::string& url)
+        :url_(url),host_port_(-1)
+    {}
 
-TEST(format,urlpaser)
+    bool parse()
+    {
+        stringparser parser(url_);
+
+        return parse_proto(parser) &&
+            parse_host(parser)     &&
+            parse_path(parser)     &&
+            parse_query(parser);
+    }
+private:
+    bool parse_proto(stringparser&  parser)
+    {
+        try
+        {
+            parser.read_until(proto_, ':')
+                .consume("//");
+        }
+        catch (const stringparser::exception&)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    bool parse_host(stringparser&  parser)
+    {
+        size_t pos = parser.pos();
+        try
+        {
+            parser.read_until(host_addr_, ':')
+                .read_int(host_port_);
+            return true;
+        }
+        catch (const stringparser::exception&)
+        {
+            parser.goto_pos(pos);
+        }
+
+        try
+        {
+            parser.read_until(host_addr_, '/');
+            parser.go_back(1);
+            return true;
+        }
+        catch (const stringparser::exception&)
+        {
+            parser.goto_pos(pos);
+        }
+
+        try
+        {
+            parser.read_until(host_addr_, '?');
+            return true;
+        }
+        catch (const stringparser::exception&)
+        {
+            parser.goto_pos(pos);
+        }
+
+        size_t pos2 = parser.pos();
+        parser.goto_pos(pos);
+        parser.read_to_pos(host_,pos2);
+        return true;
+    }
+
+    bool parse_path(stringparser&  parser)
+    {
+        if (parser.end())
+        {
+            return true;
+        }
+        if (!parser.test('/'))
+        {
+            return true;
+        }
+
+        size_t pos = parser.pos();
+        try
+        {
+            parser.read_until(path_,'?');
+            parser.go_back(1);
+        }
+        catch (const stringparser::exception&)
+        {
+            parser.goto_pos(pos);
+        }
+        return true;
+    }
+
+    bool parse_query(stringparser&  parser)
+    {
+        if (parser.end())
+        {
+            return true;
+        }
+        if (!parser.test('?'))
+        {
+            return true;
+        }
+
+        size_t pos = parser.pos();
+        std::string key;
+        std::string value;
+        while (!parser.end())
+        {
+            try
+            {
+                parser.read_until(key, '=');
+            }
+            catch (const stringparser::exception&)
+            {
+                return false;
+            }
+
+            try
+            {
+                parser.read_until(value, '&');
+                continue;
+            }
+            catch (const stringparser&)
+            {
+            }
+            value = parser.rest();
+        }
+        return true;
+    }
+
+    std::string& url_;
+    std::string proto_;
+    std::string  host_;
+    std::string host_addr_;
+    int host_port_;
+    std::string path_;
+    std::map<std::string, std::string> query_;
+};
+
+TEST(format,stringpaser)
 {
     std::string url = "rtsp://127.0.0.1:8000/file/sessions:[1]:[2]";
 
@@ -96,7 +336,9 @@ TEST(format,urlpaser)
 
     try
     {
-        stringparser(url)
+        EXPECT_TRUE(
+            stringparser(url)
+            .begin()
             .read_until(proto, ':')
             .consume("//")
             .read_until(ip, ':')
@@ -104,10 +346,10 @@ TEST(format,urlpaser)
             .consume("/file")
             .consume("/sessions")
             .consume(':')
-            .consume('[') .read_int(channel) .consume(']')
+            .consume('[').read_int(channel).consume(']')
             .consume(':')
-            .consume('[') .read_int(sub_channel) .consume(']')
-            .end();
+            .consume('[').read_int(sub_channel).consume(']')
+            .end());
     }
     catch (const stringparser::exception&)
     {
@@ -119,4 +361,10 @@ TEST(format,urlpaser)
     EXPECT_EQ(8000,port);
     EXPECT_EQ(1,channel);
     EXPECT_EQ(2,sub_channel);
+}
+
+TEST(format, urlpaser)
+{
+    //UrlParser parser("https://127.0.0.1:8000/test/1?a=1&b=2&c=ooxx");
+    //parser.parse();
 }
